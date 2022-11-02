637bf6d8e2d065f534c0ba6837b2322d825efccd
Justin Maxwell <jm00724@my.westga.edu>
2022-11-02 16:53:00 -0400

    Init commit

---
+++ notetaker.js
@@ -0,0 +1,7 @@
+/**
+ * notetaker.js implements the functionality of the note taker application:
+ * - Tracks an array with the current notes
+ * - Displays the notes on the web page
+ * - Adds notes entered by the user
+ * - Removes notes selected by the user 
+ */
